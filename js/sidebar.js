84823873758bfe4c5f8321324a5eb391b4a600cd
itsanandraj-21 <anandraj.dev21@gmail.com>
2026-07-12 17:29:32 +0530

    style change

--- js/sidebar.js
+++ js/sidebar.js
@@ -2,33 +2,74 @@ const sidebarHTML = `
 <div class="sidebar">
 
     <div class="logo-section">
-        <h1 class="logo">
-            <span>Transit</span>Ops
-        </h1>
 
-        <p>Smart Fleet Management</p>
+        <a href="index.html" class="transitops-sidebar-home-link">
+
+            <h1 class="logo">
+                <span>Transit</span>Ops
+            </h1>
+
+            <p>Smart Fleet Management</p>
+
+        </a>
+
     </div>
 
     <ul class="menu">
+
         <li><a href="dashboard.html">Dashboard</a></li>
+
         <li><a href="vehicles.html">Fleet Registry</a></li>
+
         <li><a href="drivers.html">Drivers</a></li>
+
         <li><a href="trips.html">Dispatcher</a></li>
+
         <li><a href="maintenance.html">Maintenance</a></li>
+
         <li><a href="expenses.html">Expenses</a></li>
+
         <li><a href="analytics.html">Analytics</a></li>
+
         <li><a href="settings.html">⚙ Settings</a></li>
+
+        <li>
+            <a href="#" id="logoutBtn">
+                Logout
+            </a>
+        </li>
+
     </ul>
 
 </div>
 `;
 
 document.getElementById("sidebar-container").innerHTML = sidebarHTML;
-if(
-    localStorage.getItem(
-        "loggedIn"
-    ) !== "true"
-){
-    window.location.href =
-    "login.html";
+
+/* Login Protection */
+
+if(localStorage.getItem("loggedIn") !== "true"){
+    window.location.href = "login.html";
+}
+
+/* Logout */
+
+const logoutBtn = document.getElementById("logoutBtn");
+
+if(logoutBtn){
+
+    logoutBtn.addEventListener(
+        "click",
+        function(e){
+
+            e.preventDefault();
+
+            localStorage.removeItem("loggedIn");
+            localStorage.removeItem("currentUser");
+
+            window.location.href = "login.html";
+
+        }
+    );
+
 }
